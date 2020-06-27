621f3ed953c5fb02d290a24cff0c1f194741a3a7
Sid Pagariya <sidp@umich.edu>
2020-06-27 19:26:34 -0400

    Combine ScheduleTable and Schedule

--- components/Schedule.tsx
+++ components/Schedule.tsx
@@ -1,8 +1,82 @@
 import React from 'react'
-import { Layout, Text } from '@ui-kitten/components'
-import { StyleSheet } from 'react-native'
+import {Card, Layout, Text} from '@ui-kitten/components'
+import {ScrollView, StyleSheet, View} from 'react-native'
 import { scheduleData } from '../constants/data'
-import ScheduleTable from './ScheduleTable'
+import {colors} from "../constants/custom-theme";
+
+const LectureCard = (props: any) => {
+  const lecNum = props.lec
+  const LectureHeader = (props: any) => (
+    <Layout {...props}>
+      <Text category="h5">Lecture {lecNum}</Text>
+    </Layout>
+  )
+  return props.labExam ? (
+    <Card
+      header={LectureHeader}
+      style={[
+        {
+          borderColor: colors[props.exam - 1],
+          backgroundColor: colors[props.exam - 1],
+        },
+        styles.card,
+      ]}
+    >
+      <Text category="h6">{props.labExam}</Text>
+      {props.read ? <Text>Read {props.read}</Text> : <Text>No Class!</Text>}
+    </Card>
+  ) : props.read ? (
+    <Card
+      header={LectureHeader}
+      style={[{ borderColor: colors[props.exam - 1] }, styles.card]}
+    >
+      {props.hw ? <Text category="h6">HW {props.hw} Quiz</Text> : null}
+      {props.lab ? <Text category="h6">Lab - {props.lab}</Text> : null}
+      <Text>Read {props.read}</Text>
+    </Card>
+  ) : (
+    <Card
+      header={LectureHeader}
+      style={[
+        {
+          borderColor: colors[props.exam - 2],
+          backgroundColor: colors[props.exam - 2],
+        },
+        styles.card,
+      ]}
+    >
+      <Text category="h6">
+        {props.exam === 6 ? `Final Exam (Optional)` : `Exam ${props.exam - 1}`}
+      </Text>
+      {props.read ? <Text>Read {props.read}</Text> : <Text>No Class!</Text>}
+    </Card>
+  )
+}
+
+const WeekCard = (props: any) => {
+  const weekNum = props.week
+  const WeekHeader = (props: any) => (
+    <View {...props}>
+      <Text category="h4">Week {weekNum}</Text>
+    </View>
+  )
+  return (
+    <Card header={WeekHeader} style={styles.card}>
+      {props.lectures.map((lecture: LectureData, index: number) => (
+        <LectureCard {...lecture} key={index}/>
+      ))}
+    </Card>
+  )
+}
+
+const ScheduleTable: React.FC = (props) => (
+  <ScrollView>
+    {[...scheduleData].map((weekData, index) => (
+      <WeekCard {...weekData} key={index} />
+    ))}
+
+  </ScrollView>
+)
 
 const Schedule: React.FC = () => {
   return (
@@ -18,6 +92,10 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 10,
     justifyContent: 'center',
+  },
+  card: {
+    marginTop: 8,
+    borderWidth: 3,
   },
 })
 
