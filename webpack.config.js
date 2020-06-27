8710a8909142a98be7eb4c94e32c2aabcee08eaf
Sid Pagariya <sidp@umich.edu>
2020-06-27 00:39:52 -0400

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(
+    {
+      ...env,
+      optimization: {
+        ...env.optimization,
+        minimize: true,
+      },
+      babel: {
+        dangerouslyAddModulePathsToTranspile: ['@ui-kitten/components'],
+      },
+      performance: {
+        hints: false,
+      },
+    },
+    argv
+  )
+  // config.plugins.push(
+  //   new BundleAnalyzerPlugin({
+  //     path: 'web-report',
+  //   })
+  // );
+  return config
+}
