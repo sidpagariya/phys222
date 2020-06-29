5838c4b59e6d718bcf85db8f0eb234f53c87364f
Sid Pagariya <sidp@umich.edu>
2020-06-28 22:58:45 -0400

    Add back service worker

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(
     {
       ...env,
-      offline: false,
       optimization: {
         ...env.optimization,
         minimize: true,
